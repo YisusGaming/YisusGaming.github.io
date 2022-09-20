e3d7dde74b9da15528dade8a701f8b667fc4579b
Yisus <jesusgameroom@gmail.com>
2022-09-20 12:49:24 -0400

    Added dev projects tab, new projects added

--- data/projects.js
+++ data/projects.js
@@ -7,6 +7,15 @@ const projects = [
         },
         title: 'Yisus Website',
         desc: 'Mi nuevo proyecto: <strong>esta página web</strong>! Bienvenido a mi página web, aquí encontraras datos sobre mí, información y descargas de mis proyectos ( si están disponibles ), enlaces para contactarme y más!'
+    },
+    {
+        id: 1,
+        img: {
+            src: '',
+            alt: 'Minecraft Texture Pack Creator'
+        },
+        title: 'Minecraft Texture Pack Creator',
+        desc: 'Un forma <strong>fácil</strong> y <strong>rápida</strong> de crear <strong>Paquetes de Texturas</strong> de <strong>Minecraft</strong>!<br><br>Es una <strong>aplicación de escritorio</strong> desarrollada usando <strong>Electron</strong>, la idea es publicarla para Windows 32-64 bits.<br><br><span style="color:rgba(255, 255, 0); opacity:.5;">Este proyecto sigue en desarrollo.<br>Todavía no tengo una captura de la aplicación.</span>'
     }
 ];
 
--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import projects from "../data/projects.js";
 import posts from "../data/posts.js";
+import devProjects from "../data/devprojects.js";
 
 /* Index Html */
 const postsCotainer = document.getElementById("posts-container");
@@ -33,4 +34,11 @@ const projectList = document.getElementById("project-list");
 projects.forEach((project) => {
     const template = `<div class="card bg-30"><h2 class="card-title">${project.title}</h2><div style="display: flex; justify-content: center; aling-items: center;"><img src="${project.img.src}" alt="${project.img.alt}" style="max-height: 720px; max-width: 800px;"></div><p class="card-body" style="font-size:1.5rem">${project.desc}</p></div>`;
     if (projectList) { projectList.innerHTML = template + projectList.innerHTML; }
+});
+
+/* Dev Projects */
+const devProjectList = document.getElementById("dev-project-list");
+devProjects.forEach((project) => {
+    const template = `<div class="card bg-30"><h2 class="card-title">${project.title}</h2><div style="display: flex; justify-content: center; aling-items: center;"><img src="${project.img.src}" alt="${project.img.alt}" style="max-height: 720px; max-width: 800px;"></div><p class="card-body" style="font-size:1.5rem">${project.desc}</p></div>`;
+    if (devProjectList) { devProjectList.innerHTML = template + devProjectList.innerHTML; }
 });
