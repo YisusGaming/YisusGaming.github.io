135c13f4cd5fc78dc792cd9ceb08863eb5857d22
YisusGaming <100044115+YisusGaming@users.noreply.github.com>
2022-10-27 23:24:53 -0400

    New features and Optimizing
    Removed scroll reveal in phones.
    Added timestamps to posts in the blog.
    Added new post.

--- data/posts.js
+++ data/posts.js
@@ -2,32 +2,45 @@ const posts = [
     {
         id: 0,
         title: 'Primera Publicación!',
-        body: 'Hola, bienvenido al Blog! Esta es la primera publicación, ¿has llegado en el momento justo para ver esta publicación o solo te has puesto a buscarla?'
+        body: 'Hola, bienvenido al Blog! Esta es la primera publicación, ¿has llegado en el momento justo para ver esta publicación o solo te has puesto a buscarla?',
+        date: '<span style="color: rgba(255, 0, 0, .5)">No implementado</span>'
     },
     {
         id: 1,
         title: '¿A qué me refiero cuando digo \"desarrollador intermedio\"?',
-        body: 'Seguro has notado que, en la página principal, en la carta de presentación \"<strong>¿Quién soy?</strong>\" menciono que soy un \"desarrollador <strong>intermedio</strong>\", si te preguntas a qué me refiero, aquí está la <strong>explicación</strong>:<br><br>En resumen, con esto me refiero a que, a pesar de ya tener un tiempo en todo esto, sigo aprendiendo a programar, sin embargo, <strong>yo</strong> considero que ya soy bastante bueno en todo esto de la programación, en específico, siento que tengo un nivel <strong>intermedio</strong>/alto de conocimiento en programación!<br><br><span style="color: lightcoral; opacity: .4;">Espero haberme explicado bien y haber usado los términos correctos...</span>'
+        body: 'Seguro has notado que, en la página principal, en la carta de presentación \"<strong>¿Quién soy?</strong>\" menciono que soy un \"desarrollador <strong>intermedio</strong>\", si te preguntas a qué me refiero, aquí está la <strong>explicación</strong>:<br><br>En resumen, con esto me refiero a que, a pesar de ya tener un tiempo en todo esto, sigo aprendiendo a programar, sin embargo, <strong>yo</strong> considero que ya soy bastante bueno en todo esto de la programación, en específico, siento que tengo un nivel <strong>intermedio</strong>/alto de conocimiento en programación!<br><br><span style="color: lightcoral; opacity: .4;">Espero haberme explicado bien y haber usado los términos correctos...</span>',
+        date: '<span style="color: rgba(255, 0, 0, .5)">No implementado</span>'
     },
     {
         id: 2,
         title: 'Lag...',
-        body: 'Amigos... El <strong>filter: blur(2px);</strong> da lag...'
+        body: 'Amigos... El <strong>filter: blur(2px);</strong> da lag...',
+        date: '<span style="color: rgba(255, 0, 0, .5)">No implementado</span>'
     },
     {
         id: 3,
         title: 'Listo en <br>teléfonos!',
-        body: 'Sí, así es! La página ya debería estar preparada para ser vista en teléfonos móviles. Soporta resoluciones de <strong>850px</strong> > <strong>550px!</strong>'
+        body: 'Sí, así es! La página ya debería estar preparada para ser vista en teléfonos móviles. Soporta resoluciones de <strong>850px</strong> > <strong>550px!</strong>',
+        date: '<span style="color: rgba(255, 0, 0, .5)">No implementado</span>'
+        
     },
     {
         id: 4,
         title: 'Creo que debería añadir marcas de tiempo a las publicaciones...',
-        body: 'La verdad no estoy seguro, pero añadir <strong>marcas de tiempo</strong> podría quedar bastante bien'
+        body: 'La verdad no estoy seguro, pero añadir <strong>marcas de tiempo</strong> podría quedar bastante bien',
+        date: '<span style="color: rgba(255, 0, 0, .5)">No implementado</span>'
     },
     {
         id: 5,
         title: 'Cambios en la<br/>versión de teléfonos',
-        body: 'Estoy pensado en hacer cambios en la versión para teléfonos, he estado viendo y el <strong>rendimiento</strong> en teléfonos al moverse por la página cae mucho.<br/>Probablemente elimine los efectos en teléfonos.'
+        body: 'Estoy pensado en hacer cambios en la versión para teléfonos, he estado viendo y el <strong>rendimiento</strong> en teléfonos al moverse por la página cae mucho.<br/>Probablemente elimine los efectos en teléfonos.',
+        date: '<span style="color: rgba(255, 0, 0, .5)">No implementado</span>'
+    },
+    {
+        id: 6,
+        title: 'Marcas de tiempo añadidas!',
+        body: 'Por fin, junto a otros cambios, he añadido las <strong>marcas de tiempo</strong> a las publicaciones!<br/><br/><span style="color: rgba(255, 255, 255, .5);">Las marcas de tiempo solo se muestran en la página del <a href="blog.html">Blog</a>.</span>',
+        date: '27 de Octubre de 2022.<br/>27-10-2022.'
     }
 ];
 
