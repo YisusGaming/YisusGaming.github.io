3bb1982acd705c2f5555fe35717f746854989d6a
Yisus <jesusgameroom@gmail.com>
2023-07-09 08:37:54 -0400

    added router

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <App />
+        <Router>
+            <Routes>
+                <Route path='/' element={<App />} />
+                <Route path='*' element={<h1>Not Found</h1>} />
+            </Routes>
+        </Router>
     </React.StrictMode>
 );
 
