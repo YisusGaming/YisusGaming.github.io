300519ad3e1f67343a724ae8a76ba6c7f711c5b4
Yisus <jesusgameroom@gmail.com>
2023-07-09 09:42:10 -0400

    new layout

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ export default function App() {
     return (
         <>
             <Navbar />
+            <main id='content'>
+                <h1>Content</h1>
+            </main>
         </>
     );
 }
