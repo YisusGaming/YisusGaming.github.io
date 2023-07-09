ab1b3497ea1ed5586d627be5733f880419ca5e77
Yisus <jesusgameroom@gmail.com>
2023-07-09 09:42:33 -0400

    navbar styles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import { NavLink } from 'react-router-dom';
 
 export default function Navbar() {
     return (
-        <nav>
+        <nav className='navbar'>
             <ul>
                 <li>
                     <NavLink to="/">Principal</NavLink>
