b91524e87a9ad402e9adde4abbd86df60ef18828
Yisus <jesusgameroom@gmail.com>
2023-08-19 03:27:12 -0400

    floating navbar

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,17 @@
 /* Fading the element .scroll-down depending on scroll */
-let lastScrollY = 0;
-
 document.addEventListener('scroll', (event) => {
     if (scrollY > 10) {
         document.getElementById('scroll-down').style.opacity = "0";
     } else if (scrollY <= 10) {
         document.getElementById('scroll-down').style.opacity = "1";
+    }
+});
+
+/* Configuring navbar depending on scroll */
+document.addEventListener('scroll', (event) => {
+    if (scrollY >= 125) {
+        document.querySelector('.navbar').classList.add('navbar-floating');
+    } else if (scrollY < 125) {
+        document.querySelector('.navbar').classList.remove('navbar-floating');
     }
 });
