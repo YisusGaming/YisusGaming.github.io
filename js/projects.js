08c42c3e50bbb42f3f3c4f13c18ce319648a28a3
Yisus <jesusgameroom@gmail.com>
2023-10-04 20:26:59 -0400

    repo styles

--- js/projects.js
+++ js/projects.js
@@ -5,14 +5,27 @@ fetch('https://api.github.com/users/YisusGaming/repos')
     .then((res) => res.json())
     .then((res) => {
         res.forEach((repo) => {
+            let description = "";
+            if (repo.description.charAt(repo.description.length - 1) !== '.') {
+                description = repo.description += '.';
+            } else {
+                description = repo.description;
+            }
+
             const repoTemplate = `<div class="repo">
                 <h1>
-                    <a href="${repo.html_url}">${repo.name}</a>
+                    <a target="_blank" href="${repo.html_url}">${repo.name}</a>
                 </h1>
-                <h2>${repo.description}</h2>
+                <h3>${description}</h3>
                 <div class="stats">
-                    <h4>${repo.forks_count}</h4>
-                    <h4>${repo.stargazers_count}</h4>
+                    <h4 id="forks">
+                        <i class="fa-solid fa-code-fork"></i>
+                        ${repo.forks_count}
+                    </h4>
+                    <h4 id="stars">
+                        <i class="fa-solid fa-star"></i>
+                        ${repo.stargazers_count}
+                    </h4>
                     <h4>${repo.language ?? ''}</h4>
                 </div>
             </div>`;
