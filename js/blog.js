d85a6dc9e8234e502c9088ec8b40298cf1c89e2d
Yisus <jesusgameroom@gmail.com>
2023-11-25 23:35:37 -0400

    displaying resources

--- js/blog.js
+++ js/blog.js
@@ -10,7 +10,16 @@ posts.innerHTML = `<h1 id="loading-posts">Cargando publicaciones...</h1>`;
 fetch(FETCH_URL)
     .then((res) => res.json())
     .then((res) => {
-        console.log(res); // TODO: Display posts
+        posts.innerHTML = ``;
+        res.forEach((post) => {
+            const postTemplate = `<div class="post">
+                <h1>${post.title}</h1>
+                <div id="body">${post.body}</div>
+                <h3>${post.date}</h3>
+                </div>`;
+
+            posts.innerHTML += postTemplate;
+        });
     })
     .catch((err) => {
         console.error(err);
